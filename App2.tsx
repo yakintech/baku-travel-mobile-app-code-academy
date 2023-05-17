75750a6e5a139c7a3dd5a965a8d4463c911b23f9
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-05-17 10:25:12 +0300

    ref sample

--- App2.tsx
+++ App2.tsx
@@ -1,5 +1,5 @@
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, FlatList } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import './src/assets/i18n/i18n'
@@ -8,6 +8,7 @@ import ProductList from './src/languageSample/ProductList';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import ProductDetail from './src/languageSample/ProductDetail';
+import axios from 'axios';
 
 const AppStack = createNativeStackNavigator();
 
@@ -15,13 +16,14 @@ const AppStack = createNativeStackNavigator();
 const App2 = () => {
 
     return (
-            <NavigationContainer>
-                <AppStack.Navigator>
-                    <AppStack.Screen name="ProductList" component={ProductList} />
-                    <AppStack.Screen name="ProductDetail" component={ProductDetail} />
+        <RefSample></RefSample>
+        // <NavigationContainer>
+        //     <AppStack.Navigator>
+        //         <AppStack.Screen name="ProductList" component={ProductList} />
+        //         <AppStack.Screen name="ProductDetail" component={ProductDetail} />
 
-                </AppStack.Navigator>
-            </NavigationContainer>
+        //     </AppStack.Navigator>
+        // </NavigationContainer>
 
     )
 }
@@ -31,10 +33,35 @@ export default App2
 
 
 
-// const ExploreStack = () => {
-//   return (
-//     <Explore.Navigator>
-//       <Explore.Screen name="ExploreMain" component={ExploreMain} />
-//     </Explore.Navigator>
-//   )
-// }
+const RefSample = () => {
+
+    const [products, setproducts] = useState([])
+ 
+
+    useEffect(() => {
+
+        axios.get('https://northwind.vercel.app/api/products')
+            .then((response) => {
+                setproducts(response.data);
+            }
+            )
+
+    }, [])
+
+    let flatListRef = useRef<FlatList>(null);
+
+    const toEnd = () => {        
+        flatListRef.current?.scrollToEnd();
+    }
+
+    return (<SafeAreaView>
+        <Button onPress={() => toEnd()}>Scroll to End</Button>
+        <FlatList
+            ref={flatListRef}
+            data={products}
+            renderItem={({item}:any) => <Text style={{fontSize:30}}>{item.name}</Text>}
+        />
+    </SafeAreaView>)
+
+
+}
